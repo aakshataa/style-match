6c972ab18aded3ce2a6ce99b0d018fcaddd524b8
asyed2105 <158079846+asyed2105@users.noreply.github.com>
2024-04-02 19:32:14 -0400

    Update filitering_and_synonym.py

--- filitering_and_synonym.py
+++ filitering_and_synonym.py
@@ -1,23 +1,3 @@
-"""
-basically what this file does:
-
-1. given an image_path, it first goes through astica_description() and we are returned a description ( i edited the code to alter the
-   type/lenght of description)
-2. then it goes into fitering_out_stop_words() where stop words and adjectives are removed
-        --> in this i added a list clothes and clothes in description. so basically its taking the list that has all the stop words
-           removed and seeing if it mentions any specfic article of clothing. then after it adds it to the final returned list.
-           this is because after i run the code that filters out adjectives it SOMETIMES removes the type of clothing item
-       --> so i am adding this in after just so when we do the sim score we are also checking if we are comparing
-           [black, shiney, skirt] and not just [ black , shiney]
-3. then it goes into the synonym_extractor() and we are returned a dictionary w all the synomym with the key words
-
-
-so to do simularity_score() in graph.py:
-- we need to call steps 1-3 on the users uploaded image
-- we need to call step 2 on the zara description
-- then we gonna go through each list and see if there any similar words, and if there is then the score goes up by one
-- i was thinking to make the score out of 15? ( in the astica code i made it that the description gives max 15 words)
-"""
 import requests
 import json
 import base64
@@ -26,16 +6,10 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-
-# nltk.download('averaged_perceptron_tagger')
-
-
 def get_image_base64_encoding(image_path: str) -> str:
+    """ Function to return the base64 string representation of an image - Given by our ASTICA API
     """
-    #Function to return the base64 string representation of an image
-    *Part of API
-    """
-    with open(image_path, 'rb') as file:
+    with open(image_path, 'rb') as file: 
         image_data = file.read()
     image_extension = os.path.splitext(image_path)[1]
     base64_encoded = base64.b64encode(image_data).decode('utf-8')
@@ -43,7 +17,9 @@ def get_image_base64_encoding(image_path: str) -> str:
 
 
 def asticaAPI(endpoint, payload, timeout):
-    """*Part of API"""
+    """ The function sends a POST request to the specified endpoint, including the provided payload, with a specified timeout. 
+    It expects a response with a status code of 200 (meaning success). If the response status code is 200, it returns the response
+    data in JSON format. If the response status code is not 200, it returns a JSON object indicating an error occurred - Given by our ASTICA API"""
     response = requests.post(endpoint, data=json.dumps(payload), timeout=timeout,
                                 headers={'Content-Type': 'application/json', })
     if response.status_code == 200:
@@ -53,10 +29,9 @@ def asticaAPI(endpoint, payload, timeout):
 
 
 def astica_description(filelocation: str):
-    """*Part of API
-    Given an image and prompt, API creates a description. """
+    """ Given an image, our ASTICA API uses computer visison to create a description of it. The code was originally given by our ASTICA API but we modified it"""
     # API configurations
-    asticaAPI_key = '0F432E35-599B-4DC3-AEB7-A6FF2B4E96A0274078E18C7F0C-3476-405E-92BB-0A2FC41C441B'
+    asticaAPI_key = '0F432E35-599B-4DC3-AEB7-A6FF2B4E96A0274078E18C7F0C-3476-405E-92BB-0A2FC41C441B' # We had to get this key off of ASTICA - these keys are used identify and authenticate an app/user. 
     asticaAPI_timeout = 25  # in seconds. "gpt" or "gpt_detailed" require increased timeouts
     asticaAPI_endpoint = 'https://vision.astica.ai/describe'
     asticaAPI_modelVersion = '2.1_full'
@@ -64,8 +39,7 @@ def astica_description(filelocation: str):
     asticaAPI_input = get_image_base64_encoding(filelocation)  # use base64 image input (slower)
 
     asticaAPI_visionParams = 'gpt_detailed,describe,color,categories'  # comma separated, defaults to "all".
-    asticaAPI_gpt_prompt = 'write a detailed description of the clothing item including fabric, style, color, and cut. Do not mention background and person'  # only used if visionParams includes "gpt" or "gpt_detailed"
-    asticaAPI_prompt_length = '25'  # number of words in GPT response
+    asticaAPI_gpt_prompt = 'write a detailed description of the clothing item including fabric, style, color, and cut. Do not mention background and person'  # we can use this prompt to shape the ASTICA computer vision description
 
     # Define payload dictionary
     asticaAPI_payload = {
@@ -90,10 +64,5 @@ def astica_description(filelocation: str):
                 return long_caption
 
 def user_image_description(filelocation: str) -> str:
-
+   """ Returns our ASTICA description when given the file path from our directory"""
     return astica_description(filelocation)
-
-"""fiter = filter_out_all('blazer.png')
-print(fiter)
-s = synonym_extractor(fiter)
-print(s)"""
