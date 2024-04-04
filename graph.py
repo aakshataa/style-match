db8f8b71a37e7e980287f09ff0c97f85f06c84b3
akshata <121204544+aakshataa@users.noreply.github.com>
2024-04-03 23:49:12 -0400

    Add files via upload

--- graph.py
+++ graph.py
@@ -1,26 +1,52 @@
-"""
+"""STYLEMATCH GRAPH MODULE
+
+Module Description
+==================
+This module contains the WeightedGraph and WeightedVertex classes.
+
+Copyright and Usage Information
 ===============================
-Style Match
-===============================
-...
+
+This file is Copyright (c) 2024 Aroush Syed, Akshata Kulkarni, Rosanna Jiang, Ayushi Verma
 """
 
 from __future__ import annotations
 import uuid
 import csv
 from typing import Any
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
 
 
 class WeightedVertex:
-    """A weighted vertex."""
+    """A weighted vertex corresponding to a clothing item.
+
+    Instance Attributes:
+        - item_id: The image id
+        - item_name: The clothing item name
+        - item_description: The item description
+        - price: price of item, in USD
+        - urls: Links to images of clothing item
+        - neighbours: The vertices that are adjacent to this vertex
+        - website: The link to the product informaiton on Zara website.
+
+    Representation Invariants:
+        - self not in self.neighbours
+        - all(self in u.neighbours for u in self.neighbours)
+        - item_description != ''
+    """
     item_id: str
     item_name: str
     item_description: str
     price: float
     urls: list[str]
     neighbours: dict[WeightedVertex, float]
+    website: str
 
-    def __init__(self, item_id: str, item_name: str, item_description: str, price: float, urls: list[str]) -> None:
+    def __init__(self, item_id: str, item_name: str, item_description: str, price: float,
+                 urls: list[str], website: str) -> None:
         """Initialize a new vertex with the given item."""
         self.item_id = item_id
         self.item_name = item_name
@@ -28,6 +54,7 @@ class WeightedVertex:
         self.price = price
         self.urls = urls
         self.neighbours = {}
+        self.website = website
 
     def get_ordered_neighbours(self) -> list[WeightedVertex]:
         """Returns a list of the neighbours ordered by decreasing weights"""
@@ -35,9 +62,18 @@ class WeightedVertex:
         n = dict(sorted(self.neighbours.items(), key=lambda i: -i[1]))
         return list(n.keys())
 
+    def load_vertex_image(self) -> list[str]:
+        """Returns urls of the given colthing item"""
+        return self.urls
+
 
 class WeightedGraph:
-    """A weighted graph."""
+    """A weighted graph storing all image data from the Zara dataset.
+
+    Instance Attributes:
+        - vertices: The clothing items, mapped from corresponding item name
+
+    """
 
     vertices: dict[str, WeightedVertex]
 
@@ -45,14 +81,15 @@ class WeightedGraph:
         """Initialize an empty graph (no vertices or edges)."""
         self.vertices = {}
 
-    def add_vertex(self, item_id: str, item_name: str, item_description: str, price: float, urls: list[str]) -> None:
+    def add_vertex(self, item_id: str, item_name: str, item_description: str,
+                   price: float, urls: list[str], website: str) -> None:
         """
         Add a vertex with the given parameters to this graph.
         The new vertex is not adjacent to any other vertices.
         Do nothing if the given item is already in this graph.
         """
         if item_id not in self.vertices:
-            self.vertices[item_id] = WeightedVertex(item_id, item_name, item_description, price, urls)
+            self.vertices[item_id] = WeightedVertex(item_id, item_name, item_description, price, urls, website)
 
     def add_edge(self, item_id1: Any, item_id2: Any, weight: float = 1) -> None:
         """Add an edge between the two vertices with the given item_ids in this graph,
@@ -75,9 +112,22 @@ class WeightedGraph:
         else:
             raise ValueError
 
-    def get_neighbours(self, item_id) -> list[WeightedVertex]:
+    def get_neighbours(self, item_id: str) -> list[WeightedVertex]:
         """Returns the neighbours of the vertex with the given id ordered by decreasing weight."""
         return self.vertices[item_id].get_ordered_neighbours()
+
+    def create_clothing_item(self, item_description: str) -> WeightedVertex:
+        """Add new vertex with given parameters to the weighted graph and calculate its neighbours
+        and return its item_id"""
+
+        item_id = str(uuid.uuid4())  # generate random id
+        v = WeightedVertex(item_id, "", item_description, 0, [], '')
+        self.vertices[item_id] = v
+
+        for other_id in self.vertices:
+            create_edge(self, item_id, other_id)
+
+        return v
 
 
 def load_clothing_items(clothing_items_file: str) -> WeightedGraph:
@@ -95,18 +145,9 @@ def load_clothing_items(clothing_items_file: str) -> WeightedGraph:
 
             # create vertex for each clothing item
             urls = str_to_list(line[7])
-            g.add_vertex(line[2], line[3], line[4], float(line[5]), urls)
+            g.add_vertex(line[2], line[3], line[4], float(line[5]), urls, line[1])
 
     return g
-
-
-def create_edges(g: WeightedGraph) -> None:
-    """Creates edges between vertices in the given graph based on the similarity score between item descriptions."""
-    vertex_names = list(g.vertices.keys())
-
-    for i in range(len(vertex_names)):
-        for j in range(i + 1, len(vertex_names)):
-            create_edge(g, vertex_names[i], vertex_names[j])
 
 
 def create_edge(g: WeightedGraph, id1: str, id2: str) -> None:
@@ -118,59 +159,121 @@ def create_edge(g: WeightedGraph, id1: str, id2: str) -> None:
     if id1 == id2:
         return
 
-    threshold = 0.5     # TODO: temp
-
     v1 = g.vertices[id1]
     v2 = g.vertices[id2]
 
-    # get similarity score between two vertices
-    score = get_similarity_score(v1.item_description, v2.item_description)
+    # calculate similarity score of two vertices
+    if v1.item_name == '':
+        score = get_similarity_score(v1.item_description, v2.item_description + v2.item_name)
+    elif v2.item_name == '':
+        score = get_similarity_score(v1.item_description + v1.item_name, v2.item_description)
+    else:
+        score = get_similarity_score(v1.item_description, v2.item_description)
 
-    # if score is above a certain threshold, add edge with score as the weight
-    if score > threshold:
-        g.add_edge(v1.item_id, v2.item_id, score)
+    # add edge
+    g.add_edge(v1.item_id, v2.item_id, score)
 
 
-def get_similarity_score(text1: str, text2: str) -> float:
-    """Return the similarity score between the two given texts."""
+def get_similarity_score(user_desc: str, item_desc: str) -> float:
+    """Return the similarity score between the two given texts.
+    The similarity score is calculated as so:
+    The synonyms for each generated image description (from the user's image) are generated
+    Then item_desc is generated """
 
-    # TODO: placeholder
-    return 1
+    colours = ['black', 'white', 'gold', 'silver', 'blue', 'red', 'orange', 'yellow', 'green', 'purple', 'pink',
+               'brown', 'tan', 'beige']
+    clothes = ['shirt', 'short', 'skirt', 'dress', 'jacket', 'pants', 'leggings', 'jeans', 'top', 'bottom', 'sweater',
+               'crop top', 'vest', 'underwear', 'shorts', 'sneakers', 'shoes']
+
+    # dataset description
+    zara_txt = filter_out_data(item_desc)
+
+    score = 0
+
+    for word in zara_txt:
+        if str.lower(word) in user_desc:
+            if word in colours:
+                score += 1
+            if word in clothes:
+                score += 3
+            score += 1
+
+    if len(zara_txt) > 0:
+        return score / len(zara_txt)
+    else:
+        return 0
 
 
 def str_to_list(text: str) -> list[str]:
     """Takes in a string representation of a list of strings and converts them into a list of strings."""
 
-    lst = text[1:-1].split(",")         # remove brackets and split by comma
+    lst = text[1:-1].split(", ")         # remove brackets and split by comma
     for i in range(len(lst)):
-        lst[i] = lst[i][1:-1]           # remove quotations
+        lst[i] = lst[i][1:-1]            # remove quotations
 
     return lst
 
 
-def create_clothing_item(g: WeightedGraph, item_description: str) -> str:
-    """Add new vertex with given parameters to the weighted graph and calculate its neighbours
-    and return its item_id"""
+def synonym_extractor(phrase: str) -> list[str]:
+    """Returns a dictionary of synoymns.
+    Maps keyword to its given synonyms
+    Utilizes nltk library. """
 
-    item_id = str(uuid.uuid4())         # generate random id
-    g.add_vertex(item_id, "", item_description, 0, [])      #TODO: missing info?
+    word_lst = []
 
-    for other_id in g.vertices:
-        create_edge(g, item_id, other_id)
+    for word in phrase:
+        word_lst.append(word)
+        if word in ['black', 'white', 'blue', 'pink', 'purple', 'yellow', 'red', 'orange', 'green', 'gold', 'silver']:
+            word_lst.append(word)
+            continue
+        s = []
+        for syn in wordnet.synsets(word):
+            for lem in syn.lemmas():
+                s.append(lem.name())
+        word_lst.extend(s)
 
-    return item_id
+    return word_lst
 
 
-if __name__ == '__main__':
+def filter_out_user(user_description: str) -> list:
+    """Filtering out 'stopwords' for the user description
+    - aka words that are not important to the clothing item's description
+    """
+    clothes = ['shirt', 'short', 'skirt', 'dress', 'jacket', 'pants', 'leggings', 'jeans', 'top', 'bottom', 'sweater',
+               'crop top', 'vest', 'underwear', 'shorts', 'sneakers', 'shoes']
 
-    # create the graph using a dataset
-    graph = load_clothing_items("data/store_zara_small_women.csv")
-    create_edges(graph)
+    word_tokens = word_tokenize(user_description)
+    stop_words = set(stopwords.words('english'))
 
-    # add a new clothing item to the graph
-    item_description = "This is a black dress."
-    new_id = create_clothing_item(graph, item_description)
+    filtered_sentence = []
 
-    # get list of similar clothing items ordered by decreasing similarity
-    similar_items = graph.get_neighbours(new_id)
-    print([i.item_name for i in similar_items])
+    for w in word_tokens:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+
+    clothes_in_description = []
+    for things in filtered_sentence:
+        if things in clothes:
+            clothes_in_description.append(things)
+
+    tags = nltk.pos_tag(filtered_sentence)
+    adjectives = [word for word, t in tags if t == 'JJ']
+    return adjectives + clothes_in_description
+
+
+def filter_out_data(item_desc: str) -> list[str]:
+    """Filtering out 'stopwords' for the given zara clothing item
+    - aka words that are not important to the clothing item's description
+    """
+
+    # JUST FOR THE ZARA DESCRIPTION
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(item_desc)
+
+    filtered_sentence = []
+
+    for w in word_tokens:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+
+    return filtered_sentence
